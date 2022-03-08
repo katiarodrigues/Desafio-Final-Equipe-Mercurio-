ff9e698eabcea25da9412818f17eb9fcea773e88
Luiz Eduardo <luedu159@gmail.com>
2022-03-08 20:08:33 -0300

    criando lista view

--- DesafioFinal/Source/Controller/CoinV.swift
+++ DesafioFinal/Source/Controller/CoinV.swift
@@ -8,6 +8,57 @@
 import UIKit
 
 class CoinV: UIView {
-
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(TableViewHeader.self, forHeaderFooterViewReuseIdentifier: "header")
+        tableView.register(ListaCriptoTableViewCell.self, forCellReuseIdentifier: ListaCriptoTableViewCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .black
+        return tableView
+    }()
+    //MARk: Inicializadores
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadUIElements()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func loadUIElements(){
+        setTableView()
+    
+    }
+    private func setTableView(){
+        self.addSubview(tableView)
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+}
+extension CoinV: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header")
+        return header
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 120
+    }
+}
+extension CoinV: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ListaCriptoTableViewCell.identifier, for: indexPath)
+    
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
 }
